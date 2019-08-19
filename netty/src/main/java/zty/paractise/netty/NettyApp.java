7c9ab5bc61c1e150aa507c9fbea453ed8e6fdf8e
zhangty <465355874@qq.com>
2019-08-19 13:59:50 +0800

    构建项目基础

---
+++ netty/src/main/java/zty/paractise/netty/NettyApp.java
@@ -0,0 +1,12 @@
+package zty.paractise.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyApp.class, args);
+	}
+}
